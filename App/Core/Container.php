1e3bcd70826f1582b957c187ef91950dd4ec8cc1
vidnik <thisistyoma@gmail.com>
2022-10-08 17:29:41 +0300

    implement di-container on pure php

--- App/Core/Container.php
+++ App/Core/Container.php
@@ -1,24 +1,40 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Core;
 
-
-use App\Core\Exceptions\Container\EntityNotFoundException;
+use App\Core\Exceptions\Container\ContainerException;
+use App\Core\Exceptions\Container\NotFoundException;
+use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\ContainerInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use ReflectionException;
+use ReflectionParameter;
 
 class Container implements ContainerInterface
 {
     private array $entries = [];
 
+    /**
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundException
+     */
     public function get(string $id)
     {
-        if (!$this->has($id)){
-            throw new EntityNotFoundException('There is no class "'.$id.'" in container.');
+        if ($this->has($id)) {
+            $entry = $this->entries[$id];
+
+            if (is_callable($entry)) {
+                return $entry($this);
+            }
+
+            $id = $entry;
         }
 
-        $entry = $this->entries[$id];
-
-        return $entry($this);
+        return $this->resolve($id);
     }
 
     public function has(string $id): bool
@@ -26,7 +42,69 @@ class Container implements ContainerInterface
         return isset($this->entries[$id]);
     }
 
-    public function set(string $id, callable $concrete){
+    public function set(string $id, callable|string $concrete): void
+    {
         $this->entries[$id] = $concrete;
+    }
+
+    /**
+     * @throws NotFoundExceptionInterface
+     * @throws ReflectionException
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundException
+     */
+    public function resolve(string $id)
+    {
+        try {
+            $reflectionClass = new \ReflectionClass($id);
+        } catch(ReflectionException $e) {
+            throw new NotFoundException($e->getMessage(), $e->getCode(), $e);
+        }
+
+        if (! $reflectionClass->isInstantiable()) {
+            throw new ContainerException('Class "' . $id . '" is not instantiable');
+        }
+
+        $constructor = $reflectionClass->getConstructor();
+
+        if (! $constructor) {
+            return new $id;
+        }
+
+        $parameters = $constructor->getParameters();
+
+        if (! $parameters) {
+            return new $id;
+        }
+
+        $dependencies = array_map(
+            function (ReflectionParameter $param) use ($id) {
+                $name = $param->getName();
+                $type = $param->getType();
+
+                if (! $type) {
+                    throw new ContainerException(
+                        'Failed to resolve class "' . $id . '" because param "' . $name . '" is missing a type hint'
+                    );
+                }
+
+                if ($type instanceof \ReflectionUnionType) {
+                    throw new ContainerException(
+                        'Failed to resolve class "' . $id . '" because of union type for param "' . $name . '"'
+                    );
+                }
+
+                if ($type instanceof \ReflectionNamedType && ! $type->isBuiltin()) {
+                    return $this->get($type->getName());
+                }
+
+                throw new ContainerException(
+                    'Failed to resolve class "' . $id . '" because invalid param "' . $name . '"'
+                );
+            },
+            $parameters
+        );
+
+        return $reflectionClass->newInstanceArgs($dependencies);
     }
 }
