72c8d23ffb42681e0aaf99acdcb90a00a4d4a057
vidnik <thisistyoma@gmail.com>
2022-10-08 17:15:38 +0300

    created Attributes for routing

--- App/Core/Attributes/Post.php
+++ App/Core/Attributes/Post.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Core\Attributes;
 
-class Post
-{
+use App\Core\Enums\HttpMethod;
+use Attribute;
 
+#[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
+class Post extends Route
+{
+    public function __construct(string $routePath)
+    {
+        parent::__construct($routePath, HttpMethod::Post);
+    }
 }
--- App/Core/Attributes/Route.php
+++ App/Core/Attributes/Route.php
@@ -1,8 +1,17 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Core\Attributes;
 
-class Route
-{
+use App\Core\Attributes\RouteInterface;
+use App\Core\Enums\HttpMethod;
+use Attribute;
 
+#[Attribute(Attribute::TARGET_METHOD|Attribute::IS_REPEATABLE)]
+class Route implements RouteInterface
+{
+    public function __construct(public string $routePath, public HttpMethod $method = HttpMethod::Get)
+    {
+    }
 }
