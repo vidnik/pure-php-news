bf45c14718fe08114cce424f1c4ac74416a38179
vidnik <thisistyoma@gmail.com>
2022-10-08 19:47:53 +0300

    created base model

--- App/Core/Model.php
+++ App/Core/Model.php
@@ -1,10 +1,24 @@
 <?php
 
+declare(strict_types = 1);
+
 namespace App\Core;
 
 use PDOStatement;
 
-class Model
+abstract class Model
 {
+    protected Database $db;
 
+    public function __construct()
+    {
+        $this->db = App::db();
+    }
+
+    public function fetchLazy(PDOStatement $stmt): \Generator
+    {
+        foreach($stmt as $record) {
+            yield $record;
+        }
+    }
 }
