81928e918a9d55dae9295b6ef24260c66b4c34e5
vidnik <thisistyoma@gmail.com>
2022-10-10 01:42:17 +0300

    created basic error pages

---
+++ Views/error/403.php
@@ -0,0 +1,5 @@
+<?php
+
+# todo Implement 403 error page
+
+echo 'here will be an 403 error page (user hasn\'t permissions)';
---
+++ Views/error/500.php
@@ -0,0 +1,5 @@
+<?php
+
+# todo Implement 500 error page
+
+echo 'here will be an 500 error page';
