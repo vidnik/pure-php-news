f3bda605bd9f03e8375d5c9b0ea45b0770c2b937
vidnik <thisistyoma@gmail.com>
2022-10-10 01:45:51 +0300

    added new controllers into router

--- public/index.php
+++ public/index.php
@@ -2,14 +2,16 @@
 
 declare(strict_types = 1);
 
+use App\Controllers\AdminController;
 use App\Core\App;
-use App\Controllers\News;
-use App\Controllers\UserController;
+use App\Controllers\NewsController;
+use App\Controllers\AuthController;
 use App\Core\Container;
 use App\Core\Config;
 use App\Core\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
@@ -17,13 +19,16 @@ $dotenv->load();
 const STORAGE_PATH = __DIR__ . '/../storage';
 const VIEW_PATH = __DIR__ . '/../Views';
 
+session_start();
+
 $container = new Container();
 $router    = new Router($container);
 
 $router->registerControllerRoutes(
     [
-        News::class,
-        UserController::class
+        NewsController::class,
+        AuthController::class,
+        AdminController::class
     ]
 );
 
