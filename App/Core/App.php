9bdbe0ce227612695d87e18d35f85f3eff6b71d4
vidnik <thisistyoma@gmail.com>
2022-10-12 19:00:46 +0300

    implement asset extension for twig

--- App/Core/App.php
+++ App/Core/App.php
@@ -4,13 +4,16 @@ declare(strict_types = 1);
 
 namespace App\Core;
 
-use App\Core\Config;
-use App\Core\Container;
-use App\Core\Database;
 use App\Core\Exceptions\Routing\RouteNotFoundException;
-use App\Core\Router;
-use App\Core\View;
-
+use Dotenv\Dotenv;
+use Symfony\Bridge\Twig\Extension\AssetExtension;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\Packages;
+use Symfony\Component\Asset\PathPackage;
+use Symfony\Component\Asset\UrlPackage;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 class App
 {
@@ -18,11 +21,9 @@ class App
 
     public function __construct(
         protected Container $container,
-        protected Router $router,
-        protected array $request,
-        protected Config $config
+        protected ?Router $router = null,
+        protected array $request = [],
     ) {
-        static::$db = new Database($config->db ?? []);
     }
 
     public static function db(): Database
@@ -30,7 +31,37 @@ class App
         return static::$db;
     }
 
-    public function run()
+    public function boot(): static
+    {
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__, 2));
+        $dotenv->load();
+        $config = new Config($_ENV);
+
+        $loader = new FilesystemLoader(VIEW_PATH);
+        $twig = new Environment($loader, [
+            'cache' => STORAGE_PATH . '/cache',
+            'auto_reload' => true
+        ]);
+
+        $versionStrategy = new EmptyVersionStrategy();
+        $namedPackages = array(
+            'css' => new PathPackage('/assets/css', $versionStrategy),
+        );
+        $defaultPackage = new Package($versionStrategy);
+        $packages = new Packages($defaultPackage, $namedPackages);
+
+        $twig->addExtension(new AssetExtension($packages));
+
+        $this->container->set(Environment::class, fn()=>$twig);
+
+        static::$db = new Database($config->db ?? []);
+
+        session_start();
+
+        return $this;
+    }
+
+    public function run(): void
     {
         try {
             echo $this->router->resolve($this->request['uri'], strtolower($this->request['method']));
