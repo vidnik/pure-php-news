b191bb5ff109126f2d0cfba94b4e053061f3ae25
Artem <52609325+vidnik@users.noreply.github.com>
2022-11-08 09:11:07 +0000

    added default data to schema

--- storage/schema.sql
+++ storage/schema.sql
@@ -8,6 +8,12 @@ create table auth_group
         unique (name)
 );
 
+INSERT INTO newsphp_db.auth_group (id, name, `default`) VALUES (1, 'superuser', 0);
+INSERT INTO newsphp_db.auth_group (id, name, `default`) VALUES (2, 'staff', 0);
+INSERT INTO newsphp_db.auth_group (id, name, `default`) VALUES (3, 'active', 1);
+INSERT INTO newsphp_db.auth_group (id, name, `default`) VALUES (4, 'TESTGROUP', 0);
+INSERT INTO newsphp_db.auth_group (id, name, `default`) VALUES (7, 'Test group 123', 0);
+
 create table auth_permission
 (
     id       int auto_increment
@@ -18,7 +24,17 @@ create table auth_permission
         unique (codename)
 );
 
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (1, 'canManageOwnComments', 'Can manage own comments');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (2, 'canManageAllComments', 'Can manage all comments');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (3, 'canManageOwnNews', 'Can manage own news');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (4, 'canManageAllNews', 'Can manage all news');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (5, 'canManageUsers', 'Can manage users');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (6, 'canManagePermissions', 'Can manage permissions');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (7, 'canManageGroups', 'Can manage groups');
+INSERT INTO newsphp_db.auth_permission (id, codename, name) VALUES (8, 'canManageCategories', 'Can manage categories');
+
 create table auth_group_permissions
+
 (
     id            int auto_increment
         primary key,
@@ -32,6 +48,21 @@ create table auth_group_permissions
             on update cascade on delete cascade
 );
 
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (6, 3, 1);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (13, 4, 1);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (14, 4, 2);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (15, 4, 3);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (37, 1, 2);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (38, 1, 4);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (39, 1, 5);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (40, 1, 6);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (41, 1, 7);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (42, 1, 8);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (43, 2, 2);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (44, 2, 3);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (45, 2, 8);
+INSERT INTO newsphp_db.auth_group_permissions (id, group_id, permission_id) VALUES (46, 7, 8);
+
 create table auth_user
 (
     id       int auto_increment
@@ -44,6 +75,10 @@ create table auth_user
     constraint username
         unique (username)
 );
+
+INSERT INTO newsphp_db.auth_user (id, password, username, email) VALUES (19, '$2y$10$3o/QqfJFlJOvQbLzV/ECBuRIWFvouZ/RZeE0DRUZjIK79Z1i6LKBC', 'artem', 'test@gmail.com');
+INSERT INTO newsphp_db.auth_user (id, password, username, email) VALUES (23, '$2y$10$eAO6Ae7Px6waXnizbVDc0OU53WMFL.aiZODH0bTrYKmhKj/.nqw2W', 'test123', 'testtest@gmail.com');
+INSERT INTO newsphp_db.auth_user (id, password, username, email) VALUES (24, '$2y$10$cWb5WOY2TjICbfWD2KM/3u/LKhnVB5.rv2xfbn5ZH3txescybp/sC', 'Pure', 'pure@php.com');
 
 create table auth_user_group
 (
@@ -60,6 +95,12 @@ create table auth_user_group
         foreign key (user_id) references auth_user (id)
             on update cascade on delete cascade
 );
+
+INSERT INTO newsphp_db.auth_user_group (id, user_id, group_id) VALUES (45, 19, 1);
+INSERT INTO newsphp_db.auth_user_group (id, user_id, group_id) VALUES (46, 19, 2);
+INSERT INTO newsphp_db.auth_user_group (id, user_id, group_id) VALUES (47, 19, 3);
+INSERT INTO newsphp_db.auth_user_group (id, user_id, group_id) VALUES (37, 23, 3);
+INSERT INTO newsphp_db.auth_user_group (id, user_id, group_id) VALUES (48, 24, 3);
 
 create table news_article
 (
@@ -90,6 +131,16 @@ create table news_category
     constraint slug
         unique (slug)
 );
+
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (4, 'World', 'world', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (5, 'Ukraine', 'ukraine', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (6, 'Tech', 'tech', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (7, 'Business', 'business', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (8, 'Sciense', 'sciense', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (9, 'Health', 'health', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+INSERT INTO newsphp_db.news_category (id, name, slug, description) VALUES (10, 'Entertainment and Arts', 'entertainment-and-arts', 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.');
+
+
 
 create table news_article_category
 (
